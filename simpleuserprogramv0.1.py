c449215c876bed490e64a4f063483f51326f316b
MelonMan <73000042+MelonicOverlord@users.noreply.github.com>
2021-01-06 19:58:22 +0000

    Add files via upload

---
+++ simpleuserprogramv0.1.py
@@ -0,0 +1,43 @@
+print("Hello There!")
+print("Do You Want To Know My Lucky Number?")
+print("Well, I Don't Care If You Want To Or Not! Here It Is!")
+
+lucky = putanumberhere
+print("My Lucky Number Is",lucky,"!")
+
+print("-----------------------------------------------------")
+
+print("Anyways, Hey There! What's Your Name? I Forgot To Ask...")
+user = input()
+print("Hello,",user,"Is Your Name putanamehere")
+print("Yes Or No?")
+user = input()
+if user == ("Yes"):
+    print("I TOLD YOU NOT TO TOUCH THIS CODE!")
+if user == ("No"):
+    print("Ok, You May Continue!")
+if user == ("yes"):
+    print("I TOLD YOU NOT TO TOUCH THIS CODE!")
+if user == ("no"):
+    print("Ok, You May Continue!")
+if user == ("Y"):
+    print("I TOLD YOU NOT TO TOUCH THIS CODE!")
+if user == ("N"):
+    print("Ok, You May Continue!")
+if user == ("y"):
+    print("I TOLD YOU NOT TO TOUCH THIS CODE!")
+if user == ("n"):
+    print("Ok, You May Continue!")
+
+if user == ("No"):
+    print("This Is The End Of ThiS Code!")
+if user == ("no"):
+    print("This Is The End Of ThiS Code!")
+if user == ("M"):
+    print("This Is The End Of ThiS Code!")
+if user == (""):
+    print("This Is The End Of ThiS Code!")
+
+#Code By MelonicOverlord
+#Code Version: V0.1
+#Licence: ODbL-V1.0
